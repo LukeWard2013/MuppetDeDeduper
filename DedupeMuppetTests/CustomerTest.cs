aaab433b1679ca7742588aa2a0a05dbb07069dde
nosami <jasonimison@gmail.com>
2013-02-22 00:47:27 +0000

    added 2nd stage dedupe code

--- DedupeMuppetTests/CustomerTest.cs
+++ DedupeMuppetTests/CustomerTest.cs
@@ -17,6 +17,7 @@ namespace DedupeMuppetTests
                                                         new NameAndPostcodeDedupeStrategy(),
                                                         new PhoneNumberDedupeStrategy(),
                                                         new TruncatedNameAndPostcodeStrategy());
+
         [SetUp]
         public void SetUp()
         {
@@ -30,10 +31,10 @@ namespace DedupeMuppetTests
             var customers = new List<Company>
                 {
                     // Dupe 1 contains matches of type A and C
-                    new Company(1, "Company A", "123 London Street", "WN1 0PG", "phone1"),
+                    new Company(1, "Company A", "123 London Street", "WN1 0PG", "phone1"), // 
                     new Company(2, "Company A", "123, London St", "WN1 0PG", "phone5"),
                     // ------------------------------------------------------
-                    new Company(3, "Company A", "123, London St","Different Postcode", "phone1"),
+                    new Company(3, "Company A", "123, London St", "Different Postcode", "phone1"),
                     // ------------------------------------------------------
 
                     // Dupe 2 contains a D match
@@ -56,12 +57,26 @@ namespace DedupeMuppetTests
             _deduped = _deduper.Dedupe(customers).ToArray();
 
         }
-        
+
+        [Test]
+        public void Should_be_one_group_containing_1_2_and_3_after_second_pass()
+        {
+            var groups = new SecondStageDeduper(_deduped).Combine();
+            groups.ShouldContainGroup(1, 2, 3);
+        }
+
         [Test]
         public void Should_be_one_group_containing_1_and_2_using_name_and_postcode_match()
         {
             var group = _deduped.GetGroupContaining(1, 2);
             group.Key.Strategy.ShouldBeType<NameAndPostcodeDedupeStrategy>();
+        }
+
+        [Test]
+        public void Should_be_one_group_containing_1_and_3_using_phone_number()
+        {
+            var group = _deduped.GetGroupContaining(1, 3);
+            group.Key.Strategy.ShouldBeType<PhoneNumberDedupeStrategy>();
         }
 
         [Test]
@@ -83,13 +98,6 @@ namespace DedupeMuppetTests
         {
             var group = _deduped.GetGroupContaining(7, 8);
             group.Key.Strategy.ShouldBeType<NameAddressAndPostcodeDedupeStrategy>();
-        }
-
-        [Test]
-        public void Should_be_one_group_containing_1_and_3_using_phone_number()
-        {
-            var group = _deduped.GetGroupContaining(1, 3);
-            group.Key.Strategy.ShouldBeType<PhoneNumberDedupeStrategy>();
         }
 
         [Test]
@@ -116,6 +124,90 @@ namespace DedupeMuppetTests
         }
     }
 
+    public class SecondStageDeduper
+    {
+        private readonly IGrouping<StrategySignature, Company>[] _deduped;
+
+
+        private List<HashSet<int>> groups = new List<HashSet<int>>(); 
+        private Dictionary<int, int> groupIdContainingCompany = new Dictionary<int, int>();
+ 
+        public SecondStageDeduper(IGrouping<StrategySignature, Company>[] deduped)
+        {
+            _deduped = deduped;
+        }
+
+        public CombineGroup Combine()
+        {
+            int groupId = 0;
+            foreach (var group in _deduped)
+            {
+                int foundCompanyId = 0;
+                foreach (var company in group)
+                {
+                    if (groupIdContainingCompany.ContainsKey(company.Id))
+                    {
+                        foundCompanyId = company.Id;
+                        break;
+                    }
+                }
+                if (foundCompanyId == 0)
+                {
+                    var newGroup = new HashSet<int>(group.Select(company => company.Id));
+                    groups.Add(newGroup);
+                    foreach (var company in group)
+                    {
+                        groupIdContainingCompany.Add(company.Id, groupId);
+                    }
+                    groupId++;
+                }
+                else
+                {
+                    var foundGroupId = groupIdContainingCompany[foundCompanyId];
+                    HashSet<int> foundGroup = groups[foundGroupId];
+                    foreach (var company in group)
+                    {
+                        if (!foundGroup.Contains(company.Id))
+                        {
+                            foundGroup.Add(company.Id);
+                        }
+                        groupIdContainingCompany.Add(company.Id, groupId);
+                        
+                    }
+                }
+
+            }
+            // group 1 + 2
+            // do we have a group containing 1 or 2
+            // if yes, add these numbers to existing group and distinct
+            // if no, create a new group with these ids
+            // group 1 + 3
+            // do we have a group containing 1 or 3
+            // if yes, add these numbers to existing group and distinct
+            // if no, create a new group with these ids
+            // result = group 1 + 2 + 3
+            return new CombineGroup(groups);
+        }
+    }
+
+    public class CombineGroup
+    {
+        IEnumerable<IEnumerable<int>> _groups;
+
+        public CombineGroup(IEnumerable<IEnumerable<int>> list)
+        {
+            _groups = list;
+        }
+
+        public void ShouldContainGroup(params int[] companyIds)
+        {
+            if (_groups.Any(list => list.ArraysEqual(companyIds)))
+                return;
+            
+            throw new AssertException("fucked up");
+        }
+    }
+
     public class WrongIdsException : AssertException
     {
         public WrongIdsException(string message)
@@ -136,13 +228,16 @@ namespace DedupeMuppetTests
             throw new WrongIdsException("Could not find a group with company ids " + string.Join(", ", expectedIds));
         }
 
-        static bool ArraysEqual<T>(T[] a1, T[] a2)
+        public static bool ArraysEqual<T>(this IEnumerable<T> arg1, IEnumerable<T> arg2)
         {
+            if (arg1 == null || arg2 == null)
+                return false;
+
+            var a1 = arg1.ToArray();
+            var a2 = arg2.ToArray();
+
             if (ReferenceEquals(a1, a2))
                 return true;
-
-            if (a1 == null || a2 == null)
-                return false;
 
             if (a1.Length != a2.Length)
                 return false;
